5d5aef91021e1bddd7ce2cfd6d5918d87ec519e5
mistre83 <mistre83@gmail.com>
2016-05-15 16:14:26 +0200

    Added logger and fixed issues on server

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var session = require('express-session');
-var router = require('./router')
 var passport = require('passport');
 var config = require('./config')
+var logger = require('morgan');
 
+// Controllers
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
+var authController = require('./controllers/auth');
+var oauth2Controller = require('./controllers/oauth2');
+var clientController = require('./controllers/client');
 var fs = require('fs');
 var https = require('https');
 
@@ -15,6 +21,9 @@ var https = require('https');
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 var app = express();
+
+// Logger setup
+app.use(logger('dev'));
 
 // Set view engine to ejs
 app.set('view engine', 'ejs');
@@ -33,6 +42,38 @@ app.use(session({
 
 // Use the passport package in our application
 app.use(passport.initialize());
+
+// Routing
+var router = express.Router();
+
+// Route /beers
+router.route('/beers')
+  .post(authController.isAuthenticated, beerController.postBeers)
+  .get(authController.isAuthenticated, beerController.getBeers);
+
+// Route /beers/:beer_id
+router.route('/beers/:beer_id')
+  .get(authController.isAuthenticated, beerController.getBeer)
+  .put(authController.isAuthenticated, beerController.putBeer)
+  .delete(authController.isAuthenticated, beerController.deleteBeer);
+
+  // Route /users
+  router.route('/users')
+    .post(userController.postUsers)
+    .get(authController.isAuthenticated, userController.getUsers);
+
+  // Route /clients
+  router.route('/clients')
+    .post(authController.isAuthenticated, clientController.postClients)
+    .get(authController.isAuthenticated, clientController.getClients);
+
+  /****** OAUTH2 ROUTER ******/
+  router.route('/oauth2/authorize')
+    .get(authController.isAuthenticated, oauth2Controller.authorization)
+    .post(authController.isAuthenticated, oauth2Controller.decision);
+
+  router.route('/oauth2/token')
+    .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 // Register all our routes with /api
 app.use('/api', router);
